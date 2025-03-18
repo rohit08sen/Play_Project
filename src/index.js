877a475163cc3a04def93fb63780684fce5eebaf
Rohit Senapati <rohitsenapati08@gmail.com>
2025-03-18 17:14:17 +0530

    Connect to MongoDb Atlas

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require("dotenv").config({path:'./env'});
+
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+  path:'./env'
+})
+
+
+connectDB()
+
+//Approach ---1
+/*
+  const app = express();
+  ; (async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+      app.on("errorr", (error) => {//express baat nhi kar paa raha hai db se
+        console.log("ERR:", error);
+        throw error
+      })
+
+      app.listen(process.env.PORT, () => {
+        console.log(`App is running on http://localhost:${process.env.PORT}`)
+      })
+
+
+    } catch (error) {
+      console.log("ERROR:", error)
+      throw err
+    }
+  })()
+*/
+
+
