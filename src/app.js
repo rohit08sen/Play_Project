4f4989d55be2c349468db7c7e97f05b86f8caa94
Rohit Senapati <rohitsenapati08@gmail.com>
2025-03-19 13:01:11 +0530

    #

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "16kb" }))//forms se data aya json form se
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));//url se data aye to like jaise space ka value url mein %20 hot hai to usko encode karna padega
-app.use(express.static("public"))//kuch bhi file photo aya to file mein dal do
+app.use(express.static("public"))//kuch bhi file css photo aya to file mein dal do
 app.use(cookieParser())
 
 
