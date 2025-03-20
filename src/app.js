e4983b89aa727e016ff8e38712561f36992f2f60
Rohit Senapati <rohitsenapati08@gmail.com>
2025-03-20 18:01:10 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -18,6 +18,16 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));//url se data aye
 app.use(express.static("public"))//kuch bhi file css photo aya to file mein dal do
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)///api/v1 standard writing
+/*http://localhost:8000/api/v1users--->it will direct to userRouter then if we put "/users/register" it will goto register method*/
+
+
+
 
 
 
